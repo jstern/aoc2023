2f414846cad03d2efdf563a9cc8e6f455fc6faf0
Jon Stern <jonathan.a.stern@gmail.com>
2023-11-07 07:47:19 -0600

    format/whitespace

--- solution/y2022d1.go
+++ solution/y2022d1.go
@@ -2,8 +2,7 @@ package solution
 
 func init() {
 	register("2022:1:1", y2022d1part1)
-    register("2022:1:2", y2022d1part2)
-
+	register("2022:1:2", y2022d1part2)
 }
 
 func y2022d1part1(input string) string {
